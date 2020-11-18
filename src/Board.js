f49b4b257660f1e598ffa3ea9306b57f532fa651
unknown <jreidmke@gmail.com>
2020-11-18 07:51:43 -0600

    completed checkboard function

--- src/Board.js
+++ src/Board.js
@@ -49,6 +49,9 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   function hasWon() {
     // TODO: check the board in state to determine whether the player has won.
     //so what we need to do is spread that whole array out and make sure the entire thing is true useing filter
+    let check = (setBoard(board.flat()).filter(x => x === false).length) === 0;
+    console.log(check);
+    return (setBoard(board.flat()).filter(x => x === false).length) === 0;
   }
 
   function flipCellsAround(coord) {
